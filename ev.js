64b1d2644a5fb7b522392ebd62333a0e5d7bdebf
Samvel <samvel.manukyan.1@mail.ru>
2017-05-18 20:47:13 +0400

    Yesss

--- ev.js
+++ ev.js
@@ -1,13 +1,15 @@
 'use strict';
-let counter = 0;
-const interval = setInterval(
-  function(){
-    console.log(  `Called ${++counter} times`);
-    if(counter == 7)
-    {
-      clearInterval(interval);
-    }
-  },3000
-
-
-)
+// let counter = 0;
+// const interval = setInterval(
+//   function(){
+//     console.log(  `Called ${++counter} times`);
+//     if(counter == 7)
+//     {
+//       clearInterval(interval);
+//     }
+//   },3000
+// )
+process.stdin.on('data', function(d){
+    console.log(d);
+    console.log(d.toString().toUpperCase())
+})
